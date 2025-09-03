3eae85583af7dcee874dc0240165a2aedeb5c084
Cavon <cavonxx@gmail.com>
2025-09-03 15:24:35 +0800

    build: add -y flag to docker installation command
    Automate the installation process by adding the -y flag to avoid manual confirmation prompts during package installation.

--- install_docker.sh
+++ install_docker.sh
@@ -12,6 +12,6 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo docker run hello-world
