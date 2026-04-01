96f79237f02e5ecbd33a594799961616319082a7
Cavon <cavonxx@gmail.com>
2026-04-01 10:13:35 +0800

    refactor(installer): improve installation script structure
    - Reorder package installation before product selection
    - Replace direct git clone with CLONE function
    - Remove unnecessary sudo from python command

--- install_pironman5.sh
+++ install_pironman5.sh
@@ -18,6 +18,7 @@ source installer.sh
 rm installer.sh
 
 installer_check_root_privileges
+installer_update_git_urls
 
 # Product selection
 echo "Please select your product:"
@@ -31,13 +32,15 @@ select choice in "${products[@]}"; do
     done
 done
 
-apt-get install git python3 python3-pip python3-setuptools -y
-
 installer_log_title "\nPreparing installation for ${product_name}"
+
+apt-get install git python3 python3-pip python3-setuptools -y
 
 cd ~
 rm -rf ~/pironman5
-git clone -b $branch_name https://github.com/sunfounder/pironman5.git --depth 1
+# git clone -b $branch_name https://github.com/sunfounder/pironman5.git --depth 1
+CLONE pironman5 $branch_name
 cd ~/pironman5
-sudo python3 install.py
+
+python3 install.py
 
